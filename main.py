b89e8f5b7dd3b22db3cfef36db482a95209852ba
user <user@example.com>
2026-09-17 11:20:38 +0900

    スイッチが押されている場合にprintで状態を表示するように

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ while (True):
     try:
         x_position = x_avg.add_val(joystick.get_x_ratio()) * 7
         y_position = y_avg.add_val(joystick.get_y_ratio()) * 7
+        
+        print(f"X座標: {x_position} Y座標: {y_position}")
+        if (joystick.is_sw_pressed()):
+            print("スイッチが押さています")
 
         led_matrix.draw(x_position, y_position)
         sleep(0.01)
